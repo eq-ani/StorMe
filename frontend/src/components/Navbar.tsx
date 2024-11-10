f12eff55bd81bafccecbffa040a0d2de8850a965
Gordan Liu <55120935+gordanliu@users.noreply.github.com>
2024-11-09 21:41:46 -0500

    feat: add search bar centered in the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,9 +5,13 @@ const Navbar: React.FC = () => {
     <nav className="absolute top-0 left-0 w-full h-24 z-10 flex items-center justify-between hover:bg-black hover:bg-opacity-15 transition-all duration-400 ease-in-out py-4 px-8  text-white">
       <div className="h-24 flex items-center gap-4">
         <a href="/" className="">
-          <img src = "/assets/images/hackathon-logo.svg" alt="StorMe Logo" className="h-12 w-auto" />
+          <img
+            src="/assets/images/hackathon-logo.svg"
+            alt="StorMe Logo"
+            className="h-12 w-auto"
+          />
         </a>
-        <p className="text-xl">StorMe</p>
+        <p className="text-xl text-[#212721]">StorMe</p>
       </div>
 
       <div className="hidden md:block">
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Search: React.FC = () => {
-  return <div>Search</div>;
+  return (
+    <div className="relative h-screen">
+      <Navbar />
+      <div className="absolute z-20 top-8 left-1/2 transform -translate-x-1/2 text-white w-1/4">
+        <input
+          type="text"
+          placeholder="Find your institution.."
+          className="w-full py-2 px-4 rounded-full bg-stone-200 text-black text-sm"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
