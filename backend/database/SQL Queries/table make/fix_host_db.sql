986a14007b48fae7e44f0a3dba76ee3b63abd39c
omcglynn <owenemcg@gmail.com>
2024-11-09 21:45:38 -0500

    added a query to add the extra rows to host_db that we forgot

---
+++ backend/database/SQL Queries/table make/fix_host_db.sql
@@ -0,0 +1,9 @@
+--select * from storme.host_db
+
+ALTER TABLE storme.host_db
+ADD school VARCHAR(255),
+ADD active BOOLEAN
+ADD temp_control BOOLEAN,
+ADD allergen_free BOOLEAN,
+ADD big_access BOOLEAN,
+ADD accessibility INTEGER;;
