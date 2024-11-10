6b926285be4bccf9172addd445a128cbd21d3634
Gordan Liu <55120935+gordanliu@users.noreply.github.com>
2024-11-10 08:14:55 -0500

    style: added new categories to property page listings

--- frontend/src/pages/PropertyPage.tsx
+++ frontend/src/pages/PropertyPage.tsx
@@ -3,7 +3,19 @@ import { Link, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Property } from "../types/Property";
 import { people } from "../types/people"; // Updated to import People type
-import { ArrowLeft, Calendar, Mail, MapPin, Ruler, Star } from "lucide-react";
+import {
+  Accessibility,
+  AlertTriangle,
+  ArrowLeft,
+  Calendar,
+  Mail,
+  MapPin,
+  PawPrint,
+  Ruler,
+  Snowflake,
+  Sofa,
+  Star,
+} from "lucide-react";
 
 const PropertyPage: React.FC = () => {
   const [properties, setProperties] = useState<Property[]>([]);
@@ -87,41 +99,78 @@ const PropertyPage: React.FC = () => {
           </div>
 
           {/* right container */}
-          <div className="w-1/3 flex flex-col p-6 gap-6 rounded-lg shadow-md self-start">
+          <div className="w-1/3 flex flex-col gap-6 self-start">
             {/* upper div */}
-            <div className="flex items-center space-x-4">
-              <img
-                src={person.profile_picture_url}
-                alt={person.first_name + " " + person.last_name}
-                className="h-24 w-auto rounded-full object-cover"
-              />
-              <div>
-                <div className="text-xl">
-                  {person.first_name + " " + person.last_name}
+            <div className="rounded-lg shadow-md space-y-6 p-6">
+              <div className="flex items-center space-x-4">
+                <img
+                  src={person.profile_picture_url}
+                  alt={person.first_name + " " + person.last_name}
+                  className="h-24 w-auto rounded-full object-cover"
+                />
+                <div>
+                  <div className="text-xl">
+                    {person.first_name + " " + person.last_name}
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <Mail className="h-4 w-4" />
+                    <div className="text-sm">{person.email}</div>
+                  </div>
                 </div>
-                <div className="flex items-center space-x-2">
-                  <Mail className="h-4 w-4" />
-                  <div className="text-sm">{person.email}</div>
+              </div>
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <div className="bg-[gray] rounded-full px-3 py-1 text-sm text-white flex items-center">
+                    {"Rented " + prop.stays_count + " times"}
+                  </div>
+                  <div className="flex">
+                    {[...Array(5)].map((_, i) => (
+                      <Star key={i} className="h-4 w-4 fill-[#881c1c]" />
+                    ))}
+                  </div>
+                </div>
+                <blockquote className="text-sm italic text-muted-foreground">
+                  {prop.review_comm}
+                </blockquote>
+                <p className="text-sm text-muted-foreground">
+                  - Anonymous Reviewer
+                </p>
+              </div>
+            </div>
+            {/* property details */}
+            <div className="flex flex-col md:flex-row gap-4">
+              <div className="flex items-center bg-white rounded-lg shadow-md py-4 flex-1">
+                <Snowflake className="h-5 w-5 text-[#881c1c] mx-4" />
+                <div>
+                  <p className="text-sm text-gray-500">Temperature Control</p>
+                  <p className="text-lg">Yes</p>
+                </div>
+              </div>
+
+              <div className="flex items-center bg-white rounded-lg shadow-md py-4 flex-1">
+                <PawPrint className="h-5 w-5 text-[#881c1c] mx-4" />
+                <div>
+                  <p className="text-sm text-gray-500">Allergen Free</p>
+                  <p className="text-lg">Yes</p>
                 </div>
               </div>
             </div>
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <div className="bg-[gray] rounded-full px-3 py-1 text-sm text-white flex items-center">
-                  {"Rented " + prop.stays_count + " times"}
-                </div>
-                <div className="flex">
-                  {[...Array(5)].map((_, i) => (
-                    <Star key={i} className="h-4 w-4 fill-[#881c1c]" />
-                  ))}
+            <div className="flex flex-col md:flex-row gap-4">
+              <div className="flex items-center bg-white rounded-lg shadow-md py-4 flex-1">
+                <Sofa className="h-5 w-5 text-[#881c1c] mx-4" />
+                <div>
+                  <p className="text-sm text-gray-500">Large Space</p>
+                  <p className="text-lg">Yes</p>
                 </div>
               </div>
-              <blockquote className="text-sm italic text-muted-foreground">
-                {prop.review_comm}
-              </blockquote>
-              <p className="text-sm text-muted-foreground">
-                - Anonymous Reviewer
-              </p>
+
+              <div className="flex items-center bg-white rounded-lg shadow-md py-4 flex-1">
+                <Accessibility className="h-5 w-5 text-[#881c1c] mx-4" />
+                <div>
+                  <p className="text-sm text-gray-500">Accessible</p>
+                  <p className="text-lg">Yes</p>
+                </div>
+              </div>
             </div>
           </div>
 
