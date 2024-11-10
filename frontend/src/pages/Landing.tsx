369222dbe106eec5409633c93e49e8e7b23a44d6
Gordan Liu <55120935+gordanliu@users.noreply.github.com>
2024-11-09 19:10:12 -0500

    feat: added content to landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Search } from "lucide-react";
 
 const Landing: React.FC = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div
+      className="h-screen w-screen flex items-center pl-16 bg-cover bg-center"
+      style={{
+        backgroundImage: `url(/assets/images/moving-boxes.jpg)`,
+      }}
+    >
+      <div className="flex flex-col items-start gap-6 bg-opacity-80 p-6 rounded-md">
+        <p className="text-6xl">Your Space, Your Storage</p>
+        <button
+          className="flex items-center justify-center p-4 bg-white text-black text-xl rounded-md w-2/5 gap-2"
+          onClick={() => {
+            window.location.href = "/search";
+          }}
+        >
+          <Search size={24} />
+          Rent a Space
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
