d6efdd181a50117b542042d89d5acc1f279d1602
omcglynn <owenemcg@gmail.com>
2024-11-10 05:15:48 -0500

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Search from "../pages/Search";
 import Signup from "../pages/Signup";
-import Property from "../pages/Property"; // Import the Property page
+import PropertyPage from "../pages/PropertyPage"; // Import the Property page
 import Login from "../pages/Login";
 import Listing from "../pages/Listing";
 
@@ -16,7 +16,7 @@ const App: React.FC = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/listing" element={<Listing />} />
-        <Route path="/property/:id" element={<Property />} />
+        <Route path="/property/:id" element={<PropertyPage />} />
       </Routes>
     </Router>
   );
