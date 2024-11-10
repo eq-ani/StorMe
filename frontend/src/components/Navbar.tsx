c94dfc4fdbd131ccd177857666da43f363cd02e1
Gordan Liu <55120935+gordanliu@users.noreply.github.com>
2024-11-09 20:00:08 -0500

    feat: create navbar and add it to landing and signup pages

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="absolute top-0 left-0 w-full h-24 z-10 flex items-center justify-between hover:bg-black hover:bg-opacity-15 transition-all duration-400 ease-in-out py-4 px-8  text-white">
+      <div className="h-24 flex items-center gap-4">
+        <a href="/" className="">
+          <img src="/logo.svg" alt="StorMe Logo" className="h-full w-auto" />
+        </a>
+        <p className="text-xl">StorMe</p>
+      </div>
+
+      <div className="hidden md:block">
+        <button
+          className="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200"
+          onClick={() => {
+            window.location.href = "/signup";
+          }}
+        >
+          Login
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
