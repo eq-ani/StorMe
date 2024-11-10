f78b87f58d3029f8c84f2887c011551a55fcc586
omcglynn <owenemcg@gmail.com>
2024-11-10 07:30:23 -0500

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:5000/api/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: "omcglynn@umass.edu", password: "password" }),
-      });
