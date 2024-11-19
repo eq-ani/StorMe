e8e6c87d3e35c8de7fc863f803df6af97f64a98f
omcglynn <owenemcg@gmail.com>
2024-11-19 00:32:14 -0500

    Update truncate_all.sql

--- backend/database/SQL Queries/truncate_all.sql
+++ backend/database/SQL Queries/truncate_all.sql
@@ -1,8 +1,8 @@
---truncate table storme.user_ppid restart identity cascade
+truncate table storme.user_ppid restart identity cascade
 
-drop table storme.transaction_db;
-drop table storme.reviews_db;
-drop table storme.bookings_db;
-DROP table storme.host_db;
-drop table storme.renter_db;
-drop table storme.user_ppid;
+--drop table storme.transaction_db;
+--drop table storme.reviews_db;
+--drop table storme.bookings_db;
+--DROP table storme.host_db;
+--drop table storme.renter_db;
+--drop table storme.user_ppid;
